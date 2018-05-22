8370de4dee4f45cf8b862035231498fc28aa813f
Michal Krupinski <michal.krupinski@fuerogames.pl>
2018-05-22 19:38:47 +0200

    401

--- IssuesWithNonGenericCollections/Program.cs
+++ IssuesWithNonGenericCollections/Program.cs
@@ -13,7 +13,7 @@ namespace IssuesWithNonGenericCollections
 
         {
             SimpleBoxUnboxOperation();
-            
+                        
         }
 
         static void SimpleBoxUnboxOperation()
